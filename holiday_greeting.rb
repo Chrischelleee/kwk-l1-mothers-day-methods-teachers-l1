a2b95e25ff1455eb9120f6fd98335605fd40443d
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-24 13:59:59 +0000

    automatically backed up by learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,4 @@
+def holiday_greeting(holiday="Happy Mother's Day", to="Mom", from="Your Favorite Child")
+  puts "#{holiday}, #{to}! From #{from}"
+end
+holiday_greeting("Happy Fourth of July", "Jay-Z", "Beyonce")
